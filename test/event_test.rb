3877f4f47aaa2b2aad17b062add77469e40a3fc6
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:32:55 -0600

    Add test for attributes

--- test/event_test.rb
+++ test/event_test.rb
@@ -27,4 +27,9 @@ class EventTest < Minitest::Test
     assert_instance_of Event, @event
   end
 
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @event.name
+    assert_equal [], @event.food_trucks 
+  end
+
 end
