8178c53f9e1668315ef35fbd602396b95ac89a9a
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:42:26 -0600

    Add test to list food trucks that sell item

--- test/event_test.rb
+++ test/event_test.rb
@@ -10,16 +10,16 @@ class EventTest < Minitest::Test
   def setup
     @event = Event.new("South Pearl Street Farmers Market")
     @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @food_truck1.stock(@item1, 35)
     @food_truck1.stock(@item2, 7)
-    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
     @food_truck2.stock(@item4, 50)
     @food_truck2.stock(@item3, 2)
-    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
     @food_truck3.stock(@item1, 65)
   end
 
@@ -37,6 +37,21 @@ class EventTest < Minitest::Test
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
     assert_equal [@food_truck1, @food_truck2, @food_truck3], @event.food_trucks
+  end
+
+  def test_it_can_return_food_truck_names
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal ["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @event.food_truck_names
+  end
+
+  def test_it_can_list_food_trucks_that_sell_item
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal [@foodtruck1, @food_truck3], @event.food_trucks_that_sell(@item1)
+    assert_equal [@food_truck2], @event.food_trucks_that_sell(@item4)
   end
 
 end
