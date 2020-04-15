1f1cffae0ebfab0f1707360fe4fa88ad92ee5c9e
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:07:13 -0600

    Update price attribute to return float, not string

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,9 @@
 class Item
   attr_reader :name, :price
-  
+
   def initialize(item_info)
     @name = item_info[:name]
-    @price = item_info[:price]
+    @price = item_info[:price].gsub(/[^\d\.]/, '').to_f
   end
 
 end
