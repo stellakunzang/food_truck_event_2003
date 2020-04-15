f461ffb1d5df2ff3aed6af19c840a69c5cfe516d
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:21:15 -0600

    Add to stock and check stock test to ensure 0 returned for out of stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -25,6 +25,7 @@ class FoodTruckTest < Minitest::Test
     assert_equal 0, @food_truck.check_stock(@item1)
     @food_truck.stock(@item1, 30)
     assert_equal 30, @food_truck.check_stock(@item1)
+    assert_equal 0, @food_truck.check_stock(@item2)
   end
 
   def test_it_can_add_to_stock
