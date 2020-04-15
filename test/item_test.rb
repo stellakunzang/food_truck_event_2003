3b746d23b853721d4b2c6a0c7e12e657cc2baeca
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:05:53 -0600

    Add test for atrributes

--- test/item_test.rb
+++ test/item_test.rb
@@ -11,6 +11,11 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Item, @item1 
+    assert_instance_of Item, @item1
+  end
+
+  def test_it_has_attributes
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price
   end
 end
