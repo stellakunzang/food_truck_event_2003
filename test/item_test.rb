20b6e9e050b9b04a22b949a5fca14b3f4c956d68
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:03:14 -0600

    Add test it exists

--- test/item_test.rb
+++ test/item_test.rb
@@ -8,5 +8,9 @@ class ItemTest < Minitest::Test
   def setup
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-  end 
+  end
+
+  def test_it_exists
+    assert_instance_of Item, @item1 
+  end
 end
