1a67901d44abda29e9f7a6d969738aebbeef8462
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-15 10:02:29 -0600

    Create setup for item tests

---
+++ test/item_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "pry"
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end 
+end
